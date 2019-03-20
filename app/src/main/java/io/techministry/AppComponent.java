85c5468d0b1cbf06a5ae4f3989519de328ab596c
Kevin Phillips <inquiries@kevinphillips.email>
2019-03-20 03:23:57 -0400

    copying structure over from FMC App

---
+++ app/src/main/java/io/techministry/AppComponent.java
@@ -0,0 +1,5 @@
+package io.techministry;
+
+public interface AppComponent {
+    //Todo: Add Dagger2
+}
---
+++ app/src/main/java/io/techministry/DashboardActivity.java
@@ -0,0 +1,12 @@
+package io.techministry;
+
+import android.os.Bundle;
+
+public class DashboardActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
