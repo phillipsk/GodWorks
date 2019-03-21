e297aea6e36d1f62f874f7abb06bc76472e1ec64
Kevin Phillips <inquiries@kevinphillips.email>
2019-03-21 02:55:25 -0400

    setup complete BottomNavigationView

--- app/src/main/java/io/techministry/BlankFragment.java
+++ app/src/main/java/io/techministry/BlankFragment.java
@@ -64,7 +64,7 @@ public class BlankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
+        textView.setText(R.string.app_name);
         return textView;
     }
 
